6d707251ec260af8f11aedf01a73a3f9b9c4771b
️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️ <30877561+volamtuan@users.noreply.github.com>
2024-06-09 15:01:50 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,35 +2,74 @@
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 random() {
-    tr </dev/urandom -dc A-Za-z0-9 | head -c5
-    echo
+	tr </dev/urandom -dc A-Za-z0-9 | head -c5
+	echo
 }
 
 array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
 gen64() {
-    ip64() {
-        echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
-    }
-    echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
+	ip64() {
+		echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
+	}
+	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
 
-# Kiểm tra phiên bản hệ điều hành
-os_info=$(check_os_version)
-os=$(echo "$os_info" | awk '{print $1}')
+# Hàm kiểm tra và chọn tên giao diện mạng tự động
+auto_detect_interface() {
+    INTERFACE=$(ip -o link show | awk -F': ' '$3 !~ /lo|vir|^[^0-9]/ {print $2; exit}')
+}
 
-if [ "$os" == "centos" ]; then
-    echo "Thiết lập ipv6 và cài đặt apps cho CentOS"
-    yum -y install curl wget nano gcc net-tools bsdtar zip >/dev/null 2>&1
-    # Tải script ipv6.sh và thực thi
-    wget https://raw.githubusercontent.com/volamtuan/proxyv6/main/ip.sh && chmod +x ip.sh && bash ip.sh
-elif [ "$os" == "ubuntu" ]; then
-    echo "Thiết lập ipv6 và cài đặt apps cho Ubuntu"
-    apt-get -y install wget nano gcc net-tools bsdtar zip >/dev/null 2>&1
-    # Tải script ipv6.sh và thực thi
-    wget https://raw.githubusercontent.com/volamtuan/proxyv6/main/ipv6.sh && chmod +x ipv6.sh && bash ipv6.sh
+# Get IPv6 address
+ipv6_address=$(ip addr show eth0 | awk '/inet6/{print $2}' | grep -v '^fe80' | head -n1)
+
+# Check if IPv6 address is obtained
+if [ -n "$ipv6_address" ]; then
+    echo "IPv6 address obtained: $ipv6_address"
+
+    # Declare associative arrays to store IPv6 addresses and gateways
+    declare -A ipv6_addresses=(
+        [4]="2600:2d00:$IPD:0000/64"
+        [5]="2600:2d00:$IPD:0000/64"
+        [244]="2600:2d00:$IPD:0000/64"
+        ["default"]="2600:2d00:$IPC::$IPD:0000/64"
+    )
+
+    declare -A gateways=(
+        [4]="2600:2d00:$IPC::1"
+        [5]="2600:2d00:$IPC::1"
+        [244]="2600:2d00:$IPC::1"
+        ["default"]="2600:2d00:$IPC::1"
+    )
+
+    # Get IPv4 third and fourth octets
+    IPC=$(echo "$ipv6_address" | cut -d":" -f5)
+    IPD=$(echo "$ipv6_address" | cut -d":" -f6)
+
+    # Set IPv6 address and gateway based on IPv4 third octet
+    IPV6_ADDRESS="${ipv6_addresses[$IPC]}"
+    GATEWAY="${gateways[$IPC]}"
+
+    # Check if interface is available
+    INTERFACE=$(ip -o link show | awk -F': ' '{print $2}' | grep -v lo | head -n1)
+
+    if [ -n "$INTERFACE" ]; then
+        echo "Configuring interface: $INTERFACE"
+
+        # Configure IPv6 settings
+        echo "IPV6_ADDR_GEN_MODE=stable-privacy" >> /etc/network/interfaces
+        echo "IPV6ADDR=$ipv6_address/64" >> /etc/network/interfaces
+        echo "IPV6_DEFAULTGW=$GATEWAY" >> /etc/network/interfaces
+
+        # Restart networking service
+        service networking restart
+        systemctl restart NetworkManager.service
+        ifconfig "$INTERFACE"
+        echo "Done!"
+    else
+        echo "No network interface available."
+    fi
 else
-    echo "Phiên bản hệ điều hành không được hỗ trợ."
-    exit 1
+    echo "No IPv6 address obtained."
 fi
 
 # Kiểm tra kết nối IPv6
